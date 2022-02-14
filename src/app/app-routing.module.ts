b20cb9d6dc060406b5f7c6474f17a30d4e348a96
Eugene-Oluoch <eugenemarkke@gmail.com>
2022-02-14 21:17:55 +0300

    Imported components in app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+import { UserComponent } from './components/user/user.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 const routes: Routes = [];
 
 @NgModule({
