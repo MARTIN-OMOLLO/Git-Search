2b047b937a449eff15a7255d0b8a8260faa1b1c4
Eugene-Oluoch <eugenemarkke@gmail.com>
2022-02-14 21:18:22 +0300

    Added routes to app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserComponent } from './components/user/user.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: UserComponent},
+  {path:'**',component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
