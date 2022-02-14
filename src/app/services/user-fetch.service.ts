a41426dbf6ea70590e9faa8a28034deed54705ab
Eugene-Oluoch <eugenemarkke@gmail.com>
2022-02-14 21:02:08 +0300

    Delete default app content

--- src/app/services/user-fetch.service.ts
+++ src/app/services/user-fetch.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
-
+import { Repo } from '../models/Repo';
+import { User } from '../models/User';
+import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
