da930a38da6b8215d0f4b2ca9f30b2fff24d56bc
Eugene-Oluoch <eugenemarkke@gmail.com>
2022-02-14 21:44:19 +0300

    Upload

--- src/app/services/user-fetch.service.ts
+++ src/app/services/user-fetch.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Repo } from '../models/Repo';
 import { User } from '../models/User';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 @Injectable({
   providedIn: 'root'
 })
